97df4d9c316d330b7fafc0a63b2ed6216589c2fc
krucod3 <36224699+krucod3@users.noreply.github.com>
2024-11-08 15:16:37 +0000

    Fix stop script

--- stop-demo.sh
+++ stop-demo.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+
 # Cleanup Ankaios ....
 echo "Cleaning up Ankaios ..."
-pkill ank-agent
-pkill ank-server
+pkill ank-agent || true
+pkill ank-server || true
 echo "done."
 
 # Cleanup podman
 echo "Cleaning up podman ..."
 podman stop -a >/dev/null 2>&1
-podman rm -a >/dev/null 2>&1
+podman rm -fa >/dev/null 2>&1
 podman volume rm -a >/dev/null 2>&1
 echo "done."
 
