546cb456736891015e7b23c0ace1ff4c8b75a573
krucod3 <36224699+krucod3@users.noreply.github.com>
2024-11-08 15:16:27 +0000

    Extend convenience scripts with container builds

--- restart-demo.sh
+++ restart-demo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+podman build -t ghcr.io/eclipse-sdv-hackathon-chapter-two/shift2sdv/example_app:rc1 ./example_app/
+
+podman build -t ghcr.io/eclipse-sdv-hackathon-chapter-two/shift2sdv/symphony_provider:rc1 ./symphony_provider/
+
 source /workspaces/shift2sdv/stop-demo.sh
 
 source /workspaces/shift2sdv/start-demo.sh
