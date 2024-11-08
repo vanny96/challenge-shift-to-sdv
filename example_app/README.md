e73b69c7ceec8d094e6ebca05dafe31c2f3bb010
krucod3 <36224699+krucod3@users.noreply.github.com>
2024-11-08 14:55:16 +0000

    Add basic example app that listens to eCAL

---
+++ example_app/README.md
@@ -0,0 +1,5 @@
+# Example Application
+
+The example application show how data from the vehicle can be received by an application running in the vehicle.
+
+TODO
---
+++ example_app/example_app.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2024 Elektrobit Automotive GmbH and others
+
+# This program and the accompanying materials are made available under the
+# terms of the Apache License, Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0.
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import sys, time, json
+
+import ecal.core.core as ecal_core
+from ecal.core.subscriber import StringSubscriber
+
+logger = logging.getLogger("example_app")
+stdout = logging.StreamHandler(stream=sys.stdout)
+stdout.setLevel(logging.INFO)
+logger.addHandler(stdout)
+logger.setLevel(logging.INFO)
+
+# Callback for receiving messages
+def callback(topic_name, msg, time):
+    try:
+        json_msg = json.loads(msg)
+        print(f"Received: {msg}")
+    except json.JSONDecodeError:
+        logger.error(f"Error: Could not decode message: '{msg}'")
+    except Exception as e:
+        logger.error(f"Error: {e}")
+
+if __name__ == "__main__":
+    # Initialize eCAL
+    ecal_core.initialize(sys.argv, "Example App")
+
+    # Create a subscriber that listens on the "traffic_sign_detection"
+    sub = StringSubscriber("traffic_sign_detection")
+
+    # Set the Callback
+    sub.set_callback(callback)
+    
+    # Just don't exit
+    while ecal_core.ok():
+        time.sleep(0.5)
+    
+    # finalize eCAL API
+    ecal_core.finalize()
