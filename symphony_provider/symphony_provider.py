68236641ae8c1389dd5bfdda003a0b935a4667c9
krucod3 <36224699+krucod3@users.noreply.github.com>
2024-11-14 13:28:49 +0000

    Use latest Ankaios SDK

--- symphony_provider/symphony_provider.py
+++ symphony_provider/symphony_provider.py
@@ -30,14 +30,13 @@ VEHICLE_ID = os.environ.get('VIN')
 
 # Create a new Ankaios object.
 # The connection to the control interface is automatically done at this step.
-ankaios = Ankaios()
+with Ankaios() as ankaios:
 
-
-while True:
-    # The following lines just showcase a simple interaction with Ankaios.
-    # For more information checkout out the Ankaios fleet management tutorial: 
-    # https://eclipse-ankaios.github.io/ankaios/latest/usage/tutorial-fleet-management/
-    state = ankaios.get_state(field_masks=["workloadStates"])
-    logger.info(f"Hello from vehicle {VEHICLE_ID}")
-    logger.info(f"Got the following workload execution states from Ankaios: {state.to_dict()["workload_states"]}")
-    time.sleep(3.14)
+    while True:
+        # The following lines just showcase a simple interaction with Ankaios.
+        # For more information checkout out the Ankaios fleet management tutorial: 
+        # https://eclipse-ankaios.github.io/ankaios/latest/usage/tutorial-fleet-management/
+        state = ankaios.get_state(field_masks=["workloadStates"])
+        logger.info(f"Hello from vehicle {VEHICLE_ID}")
+        logger.info(f"Got the following workload execution states from Ankaios: {state.to_dict()["workload_states"]}")
+        time.sleep(3.14)
