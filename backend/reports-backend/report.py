38ad8f53c1bfc3ebed8c55ce579d667a2b794588
Giovanni Barbaro <vanni-96@hotmail.it>
2024-11-20 15:03:00 +0100

    Fixes report

--- backend/reports-backend/report.py
+++ backend/reports-backend/report.py
@@ -1,25 +1,27 @@
+import json
+
+from flask import request
 from flask_restful import Resource
 
-class Report:
-    def __init__(self, report_id: int, data):
-        self.report_id = report_id
-        self.data = data
-
 class ReportRepo:
-    repo: dict[int, Report] = {}
+    repo: dict[int, object] = {}
     highest_id = 0
 
     def list(self):
+        return self.repo
 
-    def add(self, report: Report):
+    def add(self, report: object):
         self.highest_id = self.highest_id + 1
         self.repo[self.highest_id] = report
+
 
 class ReportsResource(Resource):
     report_repo = ReportRepo()
 
     def get(self):
-        return self.repo
+        return self.report_repo.list()
 
-    def post(self, report: Report):
-
+    def post(self):
+        report = request.json
+        self.report_repo.add(report)
+        return "OK"
