b7b7c4664a06baa30831e5564047429ea33d0697
uidu7634 <zaheer.ahmed@continental-corporation.com>
2024-11-20 15:12:27 +0100

    example flask app

--- backend/reports-backend/app.py
+++ backend/reports-backend/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
-from flask_restful import Api
-from report import ReportsResource
+# from flask import Flask
+# from flask_restful import Api
+# from report import ReportsResource
+#
+# app = Flask(__name__)
+# api = Api(app)
+#
+# api.add_resource(ReportsResource, '/reports')
+
+
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-api = Api(app)
-
-api.add_resource(ReportsResource, '/reports')
+@app.route('/echo', methods=['POST'])
+def hello():
+   return jsonify(request.json)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+
+    app.run(host='0.0.0.0', port=6002, debug=True)
