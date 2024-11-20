6cfcbe0f6862c0f736be48c015dc185dea766611
Sachin Kumar <sachinkum123567@gmail.com>
2024-11-20 19:19:36 +0100

    to_dict func added to deserialize

--- apps/log_publisher_app/report_dto.py
+++ apps/log_publisher_app/report_dto.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass, field
 from typing import List
 from datetime import datetime
 from signals import Signals
+import json
+from enum import Enum
+
 
 @dataclass
 class ReportDTO:
@@ -9,5 +12,15 @@ class ReportDTO:
     vehicle_id: str
     start_timestamp: str
     stop_timestamp: str
-    criticality_level: str
-    vehicle_dynamics: List[Signals] = field(default_factory=list)
+    criticality_level: int
+    vehicle_dynamics: List
+        
+    def to_dict(self):
+        return {
+            "schema_version": self.schema_version,
+            "vehicle_id": self.vehicle_id,
+            "start_timestamp": self.start_timestamp,
+            "stop_timestamp": self.stop_timestamp,
+            "criticality_level": self.criticality_level,
+            "vehicle_dynamics": [signal.to_dict() for signal in self.vehicle_dynamics]
+        }
